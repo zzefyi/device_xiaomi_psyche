5bc29b820ec5a7e76927e1e701c2b7506cc2b727
z <z@ze.fyi>
2024-03-20 21:23:06 +0800

    void-ui kernel path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,8 +22,8 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_psyche
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_psyche
 
 # Kernel
-# TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
-TARGET_KERNEL_CONFIG += vendor/psyche_defconfig
+TARGET_KERNEL_CONFIG += vendor/xiaomi/psyche.config
+# TARGET_KERNEL_CONFIG += vendor/psyche_defconfig
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := psyche
